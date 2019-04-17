4493ce96d4adb66cacdc499145f7fdaf25b140cd
Carlos Meraz <cmeraz@localhost.localdomain>
2019-04-17 09:43:42 -0600

    first commit

---
+++ includes/contacto.php
@@ -0,0 +1,53 @@
+<div class="modal fade" id="contacto" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Informacion de contacto:</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-6">
+              <img src="assets/img/map.png" alt="Map">
+            </div>
+              <div class="col-6">
+                <dl class="row">
+    <dt class="col-sm-3">Direccion:</dt>
+    <dd class="col-sm-9">
+      <p>
+        Carretera a La Junta Campo 34 <br>
+        Brecha al Campo 2A <br>
+        Cd. Cuauhtemoc, Chihuahua.
+      </p>
+    </dd>
+
+    <dt class="col-sm-3">Contactos:</dt>
+    <dd class="col-sm-9">
+      <p>
+        <strong>Luis F. Ordonez Marioni</strong> <br>
+        Cel: (625) 102-3607
+      </p>
+      <p>
+        <strong>Gerardo Ordonez Marioni</strong> <br>
+        Cel (625) 121-8272
+      </p>
+    </dd>
+
+    <dt class="col-sm-3">Email:</dt>
+    <dd class="col-sm-9">ventas@corralesordonez.com.mx</dd>
+
+    </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
---
+++ includes/menu.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dk">
+<div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link btn btn-secondary" href="#" data-toggle="modal" data-target="#nosotros">
+              Nosotros
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn btn-secondary" href="#" data-toggle="modal" data-target="#instalaciones">
+              Corrales
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn btn-secondary" href="#">
+              Clientes
+            </a>
+        </li>
+    </ul>
+</div>
+<div class="mx-auto order-0">
+    <a class="navbar-brand mx-auto" href="#">CORRALES ORDONEZ</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</div>
+<div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link btn btn-secondary" href="#" data-toggle="modal" data-target="#contacto">
+              Contactanos
+            </a>
+        </li>
+    </ul>
+</div>
+</nav>
